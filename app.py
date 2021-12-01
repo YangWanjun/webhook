6eb20edec6132f5912090d460e65d921f56c79b8
YangWanjun <ywjsailor@gmail.com>
2021-12-01 14:47:17 +0900

    add github

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
+import hmac
 import json
 import subprocess
+from enum import Enum
+from hashlib import sha1
+
 from flask import Flask
 from flask import request
 from json import JSONDecodeError
@@ -18,34 +22,48 @@ def deploy():
     detail = None
     command = None
     path_config = Path(BASE_DIR) / 'webhook.json'
-
+    header_signature = payload = None
+    repository_type = RepositoryType.UNKNOWN
 
     # gitlab
-    header_signature = request.headers.get('X_GITLAB_TOKEN')
-    if header_signature is None:
-        detail = "failure: no signature"
-        http_status = 403
-    elif not request.json:
-        detail = 'failure: {}'.format('no payload data')
-    else:
-        data = request.json
-        repository = data.get('project')
-        repository_name = repository.get('name')
-        ref = data.get('ref')
+    if 'X_GITLAB_TOKEN' in request.headers:
+        repository_type = RepositoryType.GITLAB
+        header_signature = request.headers.get('X_GITLAB_TOKEN', None)
+        if header_signature is None:
+            detail = "failure: no signature"
+            http_status = 403
+        elif not request.json:
+            detail = 'failure: {}'.format('no payload data')
+        else:
+            payload = request.json
+    elif 'X-Hub-Signature' in request.headers:
+        repository_type = RepositoryType.GITHUB
+        header_signature = request.headers.get('X-Hub-Signature', None)
+        if header_signature is None:
+            detail = "failure: no signature"
+            http_status = 403
+        elif not request.json:
+            detail = 'failure: {}'.format('no payload data')
+        else:
+            payload = request.json
+
+    if payload:
+        repository_name = get_repository_name(payload, repository_type)
+        branch_name = get_branch_name(payload)
         try:
             with open(path_config, encoding='utf8') as f:
                 configs = json.loads(f.read())
             for c in configs:
                 if repository_name == c.get('repository'):
-                    if ref and ref.endswith(c.get('branch')):
-                        if header_signature == c.get('token'):
+                    if branch_name == c.get('branch'):
+                        if validate_token(header_signature, c.get('token'), repository_type):
                             command = c.get('shell')
                             break
                         else:
                             detail = "failure: Wrong signature"
                             break
                     else:
-                        detail = 'repository: {}, unknown branch: {}'.format(repository_name, ref)
+                        detail = 'repository: {}, unknown branch: {}'.format(repository_name, branch_name)
         except FileNotFoundError:
             # 設定ファイルが見つからない場合
             detail = f'{path_config} not exists'
@@ -67,3 +85,43 @@ def deploy():
         'detail': detail,
         'command': command
     }, http_status
+
+
+class RepositoryType(Enum):
+    UNKNOWN = 0
+    GITLAB = 1
+    GITHUB = 2
+
+
+def get_repository_name(payload: dict, repository_type: RepositoryType) -> (str, None):
+    if repository_type == RepositoryType.GITLAB:
+        repository = payload.get('project')
+        return repository.get('name')
+    elif repository_type == RepositoryType.GITHUB:
+        repository = payload.get('repository')
+        return repository.get('name')
+    else:
+        return None
+
+
+def get_branch_name(payload: dict) -> (str, None):
+    if 'ref' in payload:
+        return payload.get('ref').replace('refs/heads/', '')
+    else:
+        return None
+
+
+def validate_token(header_signature: str, token: str, repository_type: RepositoryType) -> bool:
+    encoding = 'utf-8'
+    if repository_type == RepositoryType.GITLAB:
+        return header_signature == token
+    elif repository_type == RepositoryType.GITHUB:
+        sha_name, signature = header_signature.split('=')
+        if sha_name != 'sha1':
+            return False
+        mac = hmac.new(token.encode(encoding), msg=request.data, digestmod=sha1)
+        if not hmac.compare_digest(mac.hexdigest().encode(encoding), signature.encode(encoding)):
+            return False
+        return True
+    else:
+        return False
